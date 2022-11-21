17a79bf282ceade4c51ffd30888aa5765d488e71
WilhamsGomes <wilhamsgoomes@gmail.com>
2022-11-20 21:09:31 -0300

    subindo projeto da api

---
+++ src/mocks/users.js
@@ -0,0 +1,14 @@
+module.exports = [
+    {
+        id: 1,
+        name: 'Mateus'
+    },
+    {
+        id: 2,
+        name: 'Wilhams'
+    },
+    {
+        id: 3,
+        name: 'hugo'
+    },
+]
